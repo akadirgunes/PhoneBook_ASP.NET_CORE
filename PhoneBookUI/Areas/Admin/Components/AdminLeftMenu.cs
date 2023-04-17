85278546699753d62891d2201cb7fa466e84151e
KadirGüneş <akdrgns@gmail.com>
2023-04-17 15:18:42 +0300

    Add project files.

---
+++ PhoneBookUI/Areas/Admin/Components/AdminLeftMenu.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBookBusinessLayer.InterfacesOfManagers;
+using PhoneBookUI.Areas.Admin.Models;
+
+namespace PhoneBookUI.Areas.Admin.Components
+{
+    public class AdminLeftMenu : ViewComponent
+    {
+        private readonly IMemberManager _memberManager;
+        private readonly IPhoneTypeManager _phoneTypeManager;
+        private readonly IMemberPhoneManager _memberPhoneManager;
+
+        public AdminLeftMenu(IMemberManager memberManager, IPhoneTypeManager phoneTypeManager, IMemberPhoneManager memberPhoneManager)
+        {
+            _memberManager = memberManager;
+            _phoneTypeManager = phoneTypeManager;
+            _memberPhoneManager = memberPhoneManager;
+        }
+
+        //Eğer email gönderilicekse buraya IEmailSender eklenmelidir.
+        public IViewComponentResult Invoke()
+        {
+            try
+            {
+                AdminLeftMenuDataCountModel model = new AdminLeftMenuDataCountModel()
+                {
+                    //toplam üye sayısı
+                    TotalMemberCount=_memberManager.GetAll().Data.Count,
+
+                    //toplam telefon tipi sayısı
+                    TotalPhoneTypeCount=_phoneTypeManager.GetAll().Data.Count,
+                    
+                    //toplam numara sayısı
+                    TotalContactNumberCount=_memberPhoneManager.GetAll().Data.Count
+
+
+                };
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                //tempdata ile burda olusan hata gönderilebilir
+
+                return View(new AdminLeftMenuDataCountModel());
+            }
+        }
+
+    }
+}
---
+++ PhoneBookUI/Areas/Admin/Models/AdminLeftMenuDataCountModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PhoneBookUI.Areas.Admin.Models
+{
+    public class AdminLeftMenuDataCountModel
+    {
+        public int TotalMemberCount { get; set; }
+        public int TotalPhoneTypeCount { get; set; }
+        public int TotalContactNumberCount { get; set; }
+
+
+
+    }
+}
